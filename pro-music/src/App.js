b173574eff0fb18f586f53c095d0da0533c11fa5
Ulyana <ushinkevich@mail.ru>
2023-09-09 12:08:35 +0400

    component menu Navigation

--- pro-music/src/App.js
+++ pro-music/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import MainNav from "./components/NavMenu.jsx";
 import MainTracklist from "./components/TrackList.jsx";
 import MainSidebar from "./components/Sidebar.jsx";
 import BarPlayer from "./components/AudioPlayer.jsx";
@@ -8,35 +9,7 @@ function App() {
     <div className="wrapper">
       <div className="container">
         <main className="main">
-          <nav className="main__nav nav">
-            <div className="nav__logo logo">
-              <img className="logo__image" src="img/logo.png" alt="logo" />
-            </div>
-            <div className="nav__burger burger">
-              <span className="burger__line"></span>
-              <span className="burger__line"></span>
-              <span className="burger__line"></span>
-            </div>
-            <div className="nav__menu menu">
-              <ul className="menu__list">
-                <li className="menu__item">
-                  <a href="#" className="menu__link">
-                    Главное
-                  </a>
-                </li>
-                <li className="menu__item">
-                  <a href="#" className="menu__link">
-                    Мой плейлист
-                  </a>
-                </li>
-                <li className="menu__item">
-                  <a href="../signin.html" className="menu__link">
-                    Войти
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
+          <MainNav />
           <MainTracklist />
           <MainSidebar />
         </main>
