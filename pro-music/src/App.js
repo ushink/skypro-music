b5647a3de868135dc30ba0ed8d2e168eb8d44e3c
Ulyana <ushinkevich@mail.ru>
2023-09-09 11:35:14 +0400

    component main Sidebar

--- pro-music/src/App.js
+++ pro-music/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import MainSidebar from "./components/Sidebar.jsx";
 import BarPlayer from "./components/AudioPlayer.jsx";
 
 function App() {
@@ -644,49 +645,8 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="main__sidebar sidebar">
-            <div className="sidebar__personal">
-              <p className="sidebar__personal-name">Sergey.Ivanov</p>
-              <div className="sidebar__icon">
-                <svg alt="logout">
-                  <use xlinkHref="img/icon/sprite.svg#logout"></use>
-                </svg>
-              </div>
-            </div>
-            <div className="sidebar__block">
-              <div className="sidebar__list">
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist01.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist02.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist03.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <MainSidebar />
         </main>
-        {/* bar */}
         <BarPlayer />
         <footer className="footer"></footer>
       </div>
