c19ca339b003c2ae59fcb0f3779ca1dc4b7f919d
Ulyana <ushinkevich@mail.ru>
2023-09-15 00:50:41 +0400

    глобальное состояние useState useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import "./App.css";
+import { useState, useEffect } from 'react'
 import MainNav from "./components/NavMenu.jsx";
 import MainTracklist from "./components/TrackList.jsx";
 import MainSidebar from "./components/Sidebar.jsx";
 import BarPlayer from "./components/AudioPlayer.jsx";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!isLoaded) {
+      const timeout = setTimeout(() => {
+        setIsLoaded(true);
+      }, 5000);
+
+      return () => clearTimeout(timeout); 
+    }
+  }, [isLoaded]);
+
   return (
     <div className="wrapper">
       <div className="container">
         <main className="main">
           <MainNav />
-          <MainTracklist />
-          <MainSidebar />
+          <MainTracklist isLoaded={isLoaded}/>
+          <MainSidebar isLoaded={isLoaded}/>
         </main>
-        <BarPlayer />
+        <BarPlayer isLoaded={isLoaded}/>
         <footer className="footer"></footer>
       </div>
   </div>
