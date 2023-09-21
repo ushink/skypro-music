2363e853ac7d9ee400a6c331bf385646030b13cb
Ulyana <ushinkevich@mail.ru>
2023-09-22 02:25:03 +0400

    стилизация Tracklist

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,36 +1,40 @@
 import "./TrackList.css";
-import PlaylistContent from "../TracklistContent.jsx";
+import PlaylistContent from "../TrackListContent/TracklistContent.jsx";
 import Filter from "../TrackFilter/TrackFilter.jsx";
+import * as S from "./TrackList.styles.js";
 
 export default function MainTracklist({ isLoaded }) {
   return (
-    <div className="main__centerblock centerblock">
-      <div className="centerblock__search search">
-        <svg className="search__svg">
+    <S.MainCenterblock>
+      <S.CenterblockSearch>
+        <S.SearchSvg>
           <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-        </svg>
-        <input
-          className="search__text"
-          type="search"
-          placeholder="Поиск"
-          name="search"
-        />
-      </div>
-      <h2 className="centerblock__h2">Треки</h2>
+        </S.SearchSvg>
+        <S.SearchText type="search" placeholder="Поиск" name="search" />
+      </S.CenterblockSearch>
+      <S.CenterblockH2>Треки</S.CenterblockH2>
       <Filter />
-      <div className="centerblock__content">
-        <div className="content__title playlist-title">
-          <div className="playlist-title__col col01">Трек</div>
-          <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
-          <div className="playlist-title__col col03">АЛЬБОМ</div>
-          <div className="playlist-title__col col04">
-            <svg className="playlist-title__svg" alt="time">
-              <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
-            </svg>
-          </div>
-        </div>
+      <S.CenterblockContent>
+        <S.ContentTitle>
+          <S.PlaylistTitleCol>
+            <S.Col01>Трек</S.Col01>
+          </S.PlaylistTitleCol>
+          <S.PlaylistTitleCol>
+            <S.Col02>ИСПОЛНИТЕЛЬ</S.Col02>
+          </S.PlaylistTitleCol>
+          <S.PlaylistTitleCol>
+            <S.Col03>АЛЬБОМ</S.Col03>
+          </S.PlaylistTitleCol>
+          <S.PlaylistTitleCol>
+            <S.Col04>
+              <S.PlaylistTitleSvg alt="time">
+                <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
+              </S.PlaylistTitleSvg>
+            </S.Col04>
+          </S.PlaylistTitleCol>
+        </S.ContentTitle>
         <PlaylistContent isLoaded={isLoaded} />
-      </div>
-    </div>
+      </S.CenterblockContent>
+    </S.MainCenterblock>
   );
 }
---
+++ src/components/TrackListContent/TracklistContent.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ContentPlaylist = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  overflow-y: auto;
+`;
