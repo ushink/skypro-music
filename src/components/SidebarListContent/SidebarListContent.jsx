eac08a7c678e37c11c9da25c77d8a2730b769984
Ulyana <ushinkevich@mail.ru>
2023-09-27 16:34:25 +0400

    rout categories

--- src/components/SidebarListContent/SidebarListContent.jsx
+++ src/components/SidebarListContent/SidebarListContent.jsx
@@ -1,31 +1,35 @@
 import * as S from "./SidebarListContent.styles.js";
+import { Link } from "react-router-dom";
+
+export const Categories = [
+  {
+    id: 1,
+    playlistName: "day's playlist",
+    artwork: "img/playlist01.png",
+  },
+  {
+    id: 2,
+    playlistName: "100 dance hits",
+    artwork: "img/playlist02.png",
+  },
+  {
+    id: 3,
+    playlistName: "indie charge",
+    artwork: "img/playlist03.png",
+  },
+];
+
+const list = Categories.map((category) => (
+  <S.SidebarItem key={category.id}>
+    <Link to={`/category/${category.id}`}>
+      {" "}
+      <S.SidebarLink href="#">
+        <S.SidebarImg src={category.artwork} alt={category.playlistName} />
+      </S.SidebarLink>
+    </Link>
+  </S.SidebarItem>
+));
 
 export default function SidebarList() {
-  return (
-    <S.SidebarList>
-      <S.SidebarItem>
-        <a className="sidebar__link" href="#">
-          <img
-            className="sidebar__img"
-            src="img/playlist01.png"
-            alt="day's playlist"
-          />
-        </a>
-      </S.SidebarItem>
-      <S.SidebarItem>
-        <S.SidebarLink href="#">
-          <S.SidebarImg src="img/playlist02.png" alt="day's playlist" />
-        </S.SidebarLink>
-      </S.SidebarItem>
-      <S.SidebarItem>
-        <a className="sidebar__link" href="#">
-          <img
-            className="sidebar__img"
-            src="img/playlist03.png"
-            alt="day's playlist"
-          />
-        </a>
-      </S.SidebarItem>
-    </S.SidebarList>
-  );
+  return <S.SidebarList>{list}</S.SidebarList>;
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { MyPlaylist } from "./pages/my-playlist";
 import { NotFound } from "./pages/404";
 import { Main } from "./pages/main";
+import { Category } from "./pages/categories";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/playlist" element={<MyPlaylist />} />
+      <Route path="/category/:id" element={<Category />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
