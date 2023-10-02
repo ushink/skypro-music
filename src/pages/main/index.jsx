601da16291815a4a51e78260f181b5fae7fef170
Ulyana <ushinkevich@mail.ru>
2023-10-02 17:31:04 +0400

    поднятие стейт

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -4,6 +4,7 @@ import MainTracklist from "../../components/TrackList/TrackList.jsx";
 import MainSidebar from "../../components/Sidebar/Sidebar.jsx";
 import BarPlayer from "../../components/AudioPlayer/AudioPlayer.jsx";
 import * as S from "./styles.js";
+import { getTodos } from "../../api.js";
 
 export const Main = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -18,16 +19,36 @@ export const Main = () => {
     }
   }, [isLoaded]);
 
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    getTodos().then((todos) => {
+      setTracks(todos);
+    });
+  }, []);
+
+  const [currentTodo, setCurrentTodo] = useState(null);
+
+  const handleTodoClick = (track) => {
+    setCurrentTodo(track);
+  };
+
   return (
     <>
       <S.Wrapper>
         <S.Container>
           <S.Main>
             <MainNav />
-            <MainTracklist isLoaded={isLoaded} />
+            <MainTracklist
+              isLoaded={isLoaded}
+              tracks={tracks}
+              handleTodoClick={handleTodoClick}
+            />
             <MainSidebar isLoaded={isLoaded} />
           </S.Main>
-          <BarPlayer isLoaded={isLoaded} />
+          {currentTodo ? (
+            <BarPlayer isLoaded={isLoaded} currentTodo={currentTodo} />
+          ) : null}
           <S.Footer></S.Footer>
         </S.Container>
       </S.Wrapper>
