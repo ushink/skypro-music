c4a66bd33604f225c83cd7eeed05f4a80508a8db
Ulyana <ushinkevich@mail.ru>
2023-10-10 18:07:59 +0400

    api ф-ция регистрации

--- src/api.js
+++ src/api.js
@@ -11,3 +11,38 @@ export async function getTracks() {
   const data = await response.json();
   return data;
 }
+
+export async function getUserSignup({ email, password, username }) {
+  const response = await fetch(
+    "https://skypro-music-api.skyeng.tech/user/signup/",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+        username,
+      }),
+      headers: {
+        // API требует обязательного указания заголовка content-type, так апи понимает что мы посылаем ему json строчку в теле запроса
+        "content-type": "application/json",
+      },
+    }
+  );
+
+  if (response.status === 400) {
+    throw new Error({
+      username: ["Пользователь с таким именем уже существует."],
+      email: ["Пользователь с таким адрес электронной почты уже существует."],
+      password: [
+        "Введённый пароль слишком короткий. Он должен содержать как минимум 8 символов.",
+        "Введённый пароль слишком широко распространён.",
+        "Введённый пароль состоит только из цифр.",
+      ],
+    });
+  } else if (response.status === 500) {
+    throw new Error("Сервер сломался");
+  }
+
+  const data = await response.json();
+  return data;
+}
