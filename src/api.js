e58fb71b9ef6e7b21f3b7ea68ad3e1af9268d10a
Ulyana <ushinkevich@mail.ru>
2023-10-10 18:13:34 +0400

    api ф-ция входа (авторизации)

--- src/api.js
+++ src/api.js
@@ -46,3 +46,28 @@ export async function getUserSignup({ email, password, username }) {
   const data = await response.json();
   return data;
 }
+
+export async function getUserLogin({ email, password }) {
+  const response = await fetch(
+    "https://skypro-music-api.skyeng.tech/user/login/",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
+
+  if (response.status === 401) {
+    throw new Error("Пользователь с таким email или паролем не найден");
+  } else if (response.status === 500) {
+    throw new Error("Сервер сломался");
+  }
+
+  const data = await response.json();
+  return data;
+}
