2fe328890ee292d9219fbf68ad6057b7c03e1956
Ulyana <ushinkevich@mail.ru>
2023-10-22 12:28:13 +0400

    Разметка стр. "мой плейлист"

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,43 +1,23 @@
 import PlaylistContent from "../TrackListContent/TracklistContent.jsx";
 import Filter from "../TrackFilter/TrackFilter.jsx";
+import CenterblockSearch from "./centerblockSearch.jsx";
 import * as S from "./TrackList.styles.js";
+import ContentTitle from "./ContentTitle.jsx";
 
 export default function MainTracklist({
   tracks,
   isLoaded,
   handleTrackClick,
   addTrackError,
-  currentTrack
+  currentTrack,
 }) {
   return (
     <S.MainCenterblock>
-      <S.CenterblockSearch>
-        <S.SearchSvg>
-          <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-        </S.SearchSvg>
-        <S.SearchText type="search" placeholder="Поиск" name="search" />
-      </S.CenterblockSearch>
+      <CenterblockSearch />
       <S.CenterblockH2>Треки</S.CenterblockH2>
       <Filter />
       <S.CenterblockContent>
-        <S.ContentTitle>
-          <S.PlaylistTitleCol>
-            <S.Col01>Трек</S.Col01>
-          </S.PlaylistTitleCol>
-          <S.PlaylistTitleCol>
-            <S.Col02>ИСПОЛНИТЕЛЬ</S.Col02>
-          </S.PlaylistTitleCol>
-          <S.PlaylistTitleCol>
-            <S.Col03>АЛЬБОМ</S.Col03>
-          </S.PlaylistTitleCol>
-          <S.PlaylistTitleCol>
-            <S.Col04>
-              <S.PlaylistTitleSvg alt="time">
-                <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
-              </S.PlaylistTitleSvg>
-            </S.Col04>
-          </S.PlaylistTitleCol>
-        </S.ContentTitle>
+        <ContentTitle />
         {addTrackError ? (
           `Не удалось загрузить плейлист, попробуйте позже. ${addTrackError}`
         ) : (
--- src/pages/my-playlist/index.jsx
+++ src/pages/my-playlist/index.jsx
@@ -1,9 +1,30 @@
-import * as S from "./styles"
+import BarPlayer from "../../components/AudioPlayer/AudioPlayer";
+import MainNav from "../../components/NavMenu/NavMenu";
+import MainSidebar from "../../components/Sidebar/Sidebar";
+import Filter from "../../components/TrackFilter/TrackFilter";
+import CenterblockSearch from "../../components/TrackList/centerblockSearch";
+import ContentTitle from "../../components/TrackList/ContentTitle";
+import * as S from "./styles";
 
 export const MyPlaylist = () => {
   return (
-    <S.header>
-      <h1>My Playlist</h1>
-    </S.header>
+    <>
+      <S.Wrapper>
+        <S.Container>
+          <S.Main>
+            <MainNav />
+            <S.MainCenterblock>
+              <CenterblockSearch />
+              <S.CenterblockH2>Мой плейлист</S.CenterblockH2>
+              <Filter />
+              <ContentTitle />
+            </S.MainCenterblock>
+            <MainSidebar />
+          </S.Main>
+          <BarPlayer />
+          <S.Footer></S.Footer>
+        </S.Container>
+      </S.Wrapper>
+    </>
   );
 };
